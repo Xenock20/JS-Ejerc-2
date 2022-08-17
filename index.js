614e0a04468ca58646628f573371ddc6cff59585
Xenock20 <101891933+Xenock20@users.noreply.github.com>
2022-08-17 17:48:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const lista = [
+    "Federico",
+    17,
+    true,
+    new Date(2004, 10, 17),
+    {
+        titulo: "Virtual Hero",
+        autor: "el Rubius",
+        fecha: "15 Septiembre 2015",
+        url: "https://www.amazon.com/-/es/Ruben-Doblas-El-Rubius/dp/6070730631"
+    }
+]
+
